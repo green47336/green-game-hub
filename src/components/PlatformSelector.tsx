c483fe8a73b98c378e7b07a1fb566cadeb949a85
Alex Green <alex.green407@gmail.com>
2025-01-28 18:11:29 -0500

    platform selector fetching menu items

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,8 +1,13 @@
 import { Button } from "@chakra-ui/react";
 import { MenuContent, MenuItem, MenuRoot, MenuTrigger } from "./ui/menu";
 import { BsChevronDown } from "react-icons/bs";
+import usePlatforms from "@/hooks/usePlatforms";
 
 const PlatformSelector = () => {
+  const { data, error } = usePlatforms();
+
+  if (error) return null;
+
   return (
     <MenuRoot>
       <MenuTrigger asChild>
@@ -11,8 +16,11 @@ const PlatformSelector = () => {
         </Button>
       </MenuTrigger>
       <MenuContent>
-        <MenuItem value="pc">PC</MenuItem>
-        <MenuItem value="android">Android</MenuItem>
+        {data.map((platform) => (
+          <MenuItem value={platform.name} key={platform.id}>
+            {platform.name}
+          </MenuItem>
+        ))}
       </MenuContent>
     </MenuRoot>
   );
