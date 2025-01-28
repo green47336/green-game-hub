663961a7a34e521c9a3246f6b86a0d0cbbca680a
Alex Green <alex.green407@gmail.com>
2025-01-28 13:52:50 -0500

    image urls service to crop images as they're called

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Game } from "@/hooks/useGames";
 import { Card, HStack, Image } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   game: Game;
@@ -11,9 +12,16 @@ const GameCard = ({ game }: Props) => {
   return (
     <>
       <Card.Root borderRadius={10} overflow="hidden">
-        <Image src={game.background_image} />
+        <Image src={getCroppedImageUrl(game.background_image)} />
         <Card.Body>
-          <Card.Header fontSize="2xl">{game.name}</Card.Header>
+          <Card.Header
+            fontSize="2xl"
+            fontWeight="bold"
+            alignSelf={"center"}
+            padding={0}
+          >
+            {game.name}
+          </Card.Header>
           <HStack justifyContent="space-between">
             <PlatformIconList
               platforms={game.parent_platforms.map((p) => p.platform)}
