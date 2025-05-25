d34d2f5281d86a074d91760427304814e492923b
Alex Green <alex.green407@gmail.com>
2025-05-25 14:09:01 -0400

    restructure layout

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,40 @@
+import GameGrid from "@/components/GameGrid";
+import GameHeading from "@/components/GameHeading";
+import GenreList from "@/components/GenreList";
+import PlatformSelector from "@/components/PlatformSelector";
+import SortSelector from "@/components/SortSelector";
+import { Box, Flex, Grid, GridItem } from "@chakra-ui/react";
+
+const HomePage = () => {
+  return (
+    <>
+      <Grid
+        templateAreas={{
+          base: `"main"`,
+          lg: `"aside main"`, // 1024px
+        }}
+        templateColumns={{ base: "1fr", lg: "200px 1fr" }}
+      >
+        <GridItem
+          area="aside"
+          paddingX={5}
+          display={{ base: "none", lg: "block" }}
+        >
+          <GenreList />
+        </GridItem>
+        <GridItem area="main">
+          <Box paddingLeft={4}>
+            <GameHeading />
+            <Flex gap={2}>
+              <PlatformSelector />
+              <SortSelector />
+            </Flex>
+          </Box>
+          <GameGrid />
+        </GridItem>
+      </Grid>
+    </>
+  );
+};
+
+export default HomePage;
