3b44ca0e92397deedbd8abf33f2e9f01f1251eee
Alex Green <alex.green407@gmail.com>
2025-05-26 14:43:42 -0400

    conditionally display metacritic score definitionitem

--- src/components/GameAttributes.tsx
+++ src/components/GameAttributes.tsx
@@ -15,9 +15,11 @@ const GameAttributes = ({ game }: Props) => {
           <Text key={platform.id}>{platform.name}</Text>
         ))}
       </DefinitionItem>
-      <DefinitionItem term="Metascore">
-        <CriticScore score={game.metacritic} />
-      </DefinitionItem>
+      {game.metacritic && (
+        <DefinitionItem term="Metascore">
+          <CriticScore score={game.metacritic} />
+        </DefinitionItem>
+      )}
       <DefinitionItem term="Genres">
         {game.genres.map((genre) => (
           <Text key={genre.id}>{genre.name}</Text>
