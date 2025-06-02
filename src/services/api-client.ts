d4b1ca703717d68f93bd4004bab83cc20169015a
Alex Green <alex.green407@gmail.com>
2025-06-02 15:20:54 -0400

    changes to use proxy server for requests

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -7,10 +7,7 @@ export interface FetchResponse<T> {
 }
 
 const axiosInstance = axios.create({
-  baseURL: "https://api.rawg.io/api",
-  params: {
-    key: import.meta.env.VITE_RAWG_API_KEY,
-  },
+  baseURL: "https://green-game-hub-proxy.vercel.app/api/",
 });
 
 class APIClient<T> {
